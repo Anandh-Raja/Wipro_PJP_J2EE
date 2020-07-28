4e31bf982fcbd684d54c5dc8203e38e4716501f5
kavya1205 <42909405+kavya1205@users.noreply.github.com>
2020-07-28 19:29:35 +0530

    Create Message.java

---
+++ Spring/Spring Basics/Assignment2/Message.java
@@ -0,0 +1,14 @@
+package com.wipro.springsample;
+
+public class Message {
+	private String msg;
+	public String getMsg() {
+	return msg;	
+	}
+	public void setMsg(String msg) {
+		this.msg = msg;
+	}
+	public void display() {
+		System.out.println(msg);
+	}
+}
